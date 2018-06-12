f32708f412710402319c9e6725c1a71b29f7f682
Marcus Almeida <s030827@gmail.com>
2018-06-12 10:20:35 -0300

    Change callback on fileReader to Promise

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -14,33 +14,46 @@ class UserController {
       event.preventDefault();
       let values = this.getValues();
 
-      this.getPhoto((content)=>{
-        values.photo = content;
-        this.addLine(values);
-      });
-
+      this.getPhoto().then(
+          (content)=>{
+            values.photo = content;
+            this.addLine(values);
+          },
+          (e)=>{
+            console.error(e);
+          }
+      );
     });
   }
 
-  getPhoto(callback){
+  getPhoto(){
 
-    let fileReader = new FileReader();
+    return new Promise((resolve, reject)=>{
 
-    let elements = [...this.formEl.elements].filter(item=>{
-        if (item.name === 'photo'){
-          return item;
-        }
-    });
+      let fileReader = new FileReader();
 
-    let file = elements[0].files[0];
+      let elements = [...this.formEl.elements].filter(item=>{
+          if (item.name === 'photo'){
+            return item;
+          }
+      });
 
-    fileReader.onload = ()=>{
+      let file = elements[0].files[0];
+
+      fileReader.onload = ()=>{
+        
+        resolve(fileReader.result);
+
+      };
       
-      callback(fileReader.result);
+      fileReader.onerror = (e)=>{
+        reject(e);
+      }
 
-    };
+      fileReader.readAsDataURL(file);
 
-    fileReader.readAsDataURL(file);
+      });
+
   }
 
   getValues(){
