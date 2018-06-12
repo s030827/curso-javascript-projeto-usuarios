7846ae62a2aa4323f8e539c89b78e367268de785
Marcus Almeida <s030827@gmail.com>
2018-06-12 11:15:26 -0300

    Bug fixed on user list, change innerHTML to appendChild

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -94,21 +94,23 @@ class UserController {
 
   addLine(dataUser){
 
-    this.tableEl.innerHTML = `
-                <tr>
-                  <td><img src="${dataUser.photo}" 
-                           alt="User Image" class="img-circle img-sm"></td>
-                  <td>${dataUser.name}</td>
-                  <td>${dataUser.email}</td>
-                  <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
-                  <td>${dataUser.birth}</td>
-                  <td>
-                    <button type="button" 
-                            class="btn btn-primary btn-xs btn-flat">Editar</button>
-                    <button type="button" 
-                            class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                  </td>
-                </tr>
+    let tr = document.createElement('tr');
+
+    tr.innerHTML = `
+          <td><img src="${dataUser.photo}" 
+                   alt="User Image" class="img-circle img-sm"></td>
+          <td>${dataUser.name}</td>
+          <td>${dataUser.email}</td>
+          <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
+          <td>${dataUser.birth}</td>
+          <td>
+            <button type="button" 
+                    class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" 
+                    class="btn btn-danger btn-xs btn-flat">Excluir</button>
+          </td>
     `;
+
+    this.tableEl.appendChild(tr);
   }
 }
