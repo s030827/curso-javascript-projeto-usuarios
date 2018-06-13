4f9370e0c8e3b7d66540096c069a306b124a694d
Marcus Almeida <s030827@gmail.com>
2018-06-13 14:39:02 -0300

    Count the number of users and admins

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -113,6 +113,8 @@ class UserController {
 
     let tr = document.createElement('tr');
 
+    tr.dataset.user = JSON.stringify(dataUser);
+
     tr.innerHTML = `
           <td><img src="${dataUser.photo}" 
                    alt="User Image" class="img-circle img-sm"></td>
@@ -129,5 +131,23 @@ class UserController {
     `;
 
     this.tableEl.appendChild(tr);
+    this.updateCount();
+  }
+
+  updateCount(){
+    
+    let numberUsers = 0;
+    let numberAdmin = 0;
+
+    [...this.tableEl.children].forEach(tr=>{
+
+      numberUsers++;
+      let user = JSON.parse(tr.dataset.user);
+      if (user._admin) numberAdmin++;
+
+    });
+
+    document.querySelector("#number-users").innerHTML = numberUsers;
+    document.querySelector("#number-users-admin").innerHTML = numberAdmin;
   }
 }
