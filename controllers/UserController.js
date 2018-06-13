3b883831047202d60ad46381e6868246f8c6e5d5
Marcus Almeida <s030827@gmail.com>
2018-06-13 08:45:58 -0300

    Validate the form

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -67,8 +67,16 @@ class UserController {
   getValues(){
 
   let user = {};
+  let isValid = true;
 
   [...this.formEl.elements].forEach(function(field, index){
+
+      if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+
+        field.parentElement.classList.add('has-error');
+        isValid = false;
+
+      }
 
       if (field.name == "gender"){
 
@@ -81,6 +89,10 @@ class UserController {
           user[field.name] = field.value;
       }
   });
+
+  if (!isValid){
+    return false;
+  }
 
   return new User(
 
