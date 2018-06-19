589824e817bc2fddc60b340074f0fcfbdb90a686
Marcus Almeida <s030827@gmail.com>
2018-06-19 10:47:47 -0300

    Refactoring userController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -149,8 +149,16 @@ class UserController {
 
   addLine(dataUser){
 
-    let tr = document.createElement('tr');
+    let tr = this.getTr(dataUser);
 
+    this.tableEl.appendChild(tr);
+    this.updateCount();
+  }
+
+  getTr(dataUser, tr = null){
+
+
+    if (tr === null) tr = document.createElement('tr');
 
     tr.dataset.user = JSON.stringify(dataUser);
 
@@ -170,8 +178,7 @@ class UserController {
     `;
 
     this.addEventTr(tr);
-    this.tableEl.appendChild(tr);
-    this.updateCount();
+    return tr;
   }
 
   showPanelCreate(){
@@ -212,25 +219,11 @@ class UserController {
                 result._photo = content;
             } 
 
+            let user = new User();
+            user.loadFromJSON(result);
 
-          tr.dataset.user = JSON.stringify(result);
+            this.getTr(user, tr);
 
-          tr.innerHTML = `
-                <td><img src="${result._photo}" 
-                         alt="User Image" class="img-circle img-sm"></td>
-                <td>${result._name}</td>
-                <td>${result._email}</td>
-                <td>${(result._admin) ? "Sim" : "Não"}</td>
-                <td>${Utils.dateFormat(result._register)}</td>
-                <td>
-                  <button type="button" 
-                          class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                  <button type="button" 
-                          class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-          `;
-
-            this.addEventTr(tr);
             this.updateCount();
 
             this.formUpdateEl.reset();
